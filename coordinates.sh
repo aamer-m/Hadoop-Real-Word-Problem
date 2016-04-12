7d5fdd50725118462f89a72a7d7978161a17c128
root <root@laughingbuddha.(none)>
2016-04-12 14:20:30 +0000

    Corrected the longitude and longitude in mapper for tweets with PLACE

--- coordinates.sh
+++ coordinates.sh
@@ -5,16 +5,9 @@ hadoop fs -put mapsome.py
 hadoop fs -put reducesome.py
 hadoop fs -chmod 777 mapsome.py
 hadoop fs -chmod 777 reducesome.py
-#dT="$(date --rfc-3339=seconds)"
 dT="$(date)"
 dirName="${dT//[-,+: ]/}"
 fileName="log_"$dirName
 time hadoop jar /root/hadoop-2.7.1/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar -input /data/twitter -output $dirName -file *.py -mapper mapsome.py -reducer reducesome.py > $fileName 2>&1
-mkdir $dirName
-mv $fileName $dirName
-mkdir $dirName/input
-mkdir $dirName/output
-hadoop fs -get mapsome.py "$dirName"/input/
-hadoop fs -get reducesome.py "$dirName"/input/
-hadoop fs -get $dirName "$dirName"/output/
+hadoop fs -get $dirName
 
--- reducesome.py
+++ reducesome.py
@@ -8,9 +8,10 @@ longs_sum = 0
 coord_count = float(0)
 total_count = float(0)
 result = {}
+# reducer to calculate centroid and poportion of location
 for line in sys.stdin:
   (key,val) = line.strip().split('\t',1)
-  # if key=="coords":
+  # converting string to a json
   coordsD = json.loads(val)
   lats_sum += float(coordsD["lat"])
   longs_sum += float(coordsD["long"])
@@ -20,5 +21,7 @@ cLatLong = (0, 0)
 if coord_count:
   cLatLong = (lats_sum/float(coord_count), longs_sum/float(coord_count))  
 result['centroid'] = cLatLong
+# calcuate proportion of tweets with location to without = count(tweets without location) / count(tweets with location)
 result['proportionOfCoordsToNoCoords'] = ((total_count - coord_count)/coord_count)
+# print the centroid and proportion
 print 'result\t%s' % json.dumps(result)
